71a789ea76e781cb671d8b90677ed54ac3327ef6
rahul nivesh <rahulnivesh2017@gmail.com>
2023-01-25 12:37:42 +0530

    arer of assig1_retiurn_23.java in java

---
+++ assig1_return30_23.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+ class assig1_return30_23{
+
+public static void main(String []args){
+
+	int n1=10,n2=20;
+
+System.out.println("Enter your  number" );
+Scanner sc=new Scanner(System.in);
+ int a=sc.nextInt();
+int  b=sc.nextInt();
+int c=a+b;
+
+
+if(c>=10 && c<=20)
+{
+System.out.println("In a range  30");
+
+}
+
+else
+{
+System.out.println("not in a range");
+}
+
+}
+}
+
