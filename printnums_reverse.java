3a194f6e24565e6fb111133eccd9b4c28b8e8021
rahul nivesh <rahulnivesh2017@gmail.com>
2023-02-27 09:52:13 +0530

    print_reverse

---
+++ printnums_reverse.java
@@ -0,0 +1,18 @@
+
+// write a program to reverse of N to 1 numbers
+
+import java.util.Scanner;
+
+class printnums_reverse 
+{
+  public static void main(String []args)
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter value of N");
+    int n=sc.nextInt();
+    for (int i=n;i>=1;i--)
+    {
+      System.out.println(i);
+      }
+      }
+      }
