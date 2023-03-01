d772b4c3ab8b981799e5f074c72bbd41ca9117e1
rahul nivesh <rahulnivesh2017@gmail.com>
2023-03-01 14:16:56 +0530

    1 march program

---
+++ new program/pattern_char.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class pattern_char
+{
+ public static void main(String []args)
+ {
+
+  System.out.println("enter value of n");
+
+
+  for(int i=65;i<=69;i++)
+  {
+  for(int j=65;j<=i;j++)
+  {
+  System.out.print(i);
+  }
+  System.out.print("\n");
+  }
+  }
+  }
---
+++ new program/printnums.java
@@ -0,0 +1,17 @@
+
+
+// write a program to print 1 to N numbers
+import java.util.Scanner;
+class printnums
+{
+  public static void main(String []args)
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter value of N");
+    int n=sc.nextInt();
+    for (int i=1;i<=n;i++)
+    {
+     System.out. println(i);
+     }
+     }
+     }
