35da098c30f3e692b09ce85188bcafe894bb5c17
rahul nivesh <rahulnivesh2017@gmail.com>
2023-03-02 23:50:43 +0530

    oddeven_java

---
+++ new program/Odd_Even_java.java
@@ -0,0 +1,22 @@
+// Even and odd number
+
+import java.util.*;
+class Odd_Even_java
+{
+ public static void main(String []args)
+ {
+   int n;
+   System.out.println("Enter any number");
+   Scanner sc=new Scanner(System.in);
+   n=sc.nextInt();
+   if(n%2==0)
+   {
+    System.out.print("Even number");
+    }
+    else
+    {
+    System.out.print("Odd number");
+    }
+}
+}
+
