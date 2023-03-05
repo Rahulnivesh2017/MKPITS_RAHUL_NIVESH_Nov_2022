7bc9d0c334a24cc93aef72144844cc4983684786
rahul nivesh <rahulnivesh2017@gmail.com>
2023-03-05 20:30:38 +0530

    Age

---
+++ new program/Age.java
@@ -0,0 +1,16 @@
+// write a java program that takes an age (for example 20)as input and prints something as "you look older than 20".
+
+class Age{
+
+public static void main(String []args){
+
+int age=20;
+
+if (age==20)
+{
+
+System.out.println("you look older than 20");
+
+}
+}
+}
