bc073c2b015f1ce104d9ac2fd9f56a30d96549d2
rahul nivesh <rahulnivesh2017@gmail.com>
2023-03-07 02:09:15 +0530

    pattern_problems

---
+++ new program/pattern_reverse_number_j1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class pattern_reverse_number_j1
+{
+ public static void main(String []args)
+ {
+
+ System.out.println("enter value of 5");
+
+
+  for(int i=5;i>=1;i--)
+  {
+  for(int j=1;j<=i;j++)
+  {
+  System.out.print(j);
+  }
+  System.out.println("");
+  }
+  }
+  }
