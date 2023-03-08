264c66cbe67f3238c3b42d43ad934e0f0aef8ea4
rahul nivesh <rahulnivesh2017@gmail.com>
2023-03-08 23:03:16 +0530

    pattern_reverse_char1

---
+++ new program/pattern_reverse_char1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class pattern_reverse_char1{
+
+ public static void main(String []args)
+ {
+
+ System.out.println("enter value of 5");
+
+
+  for(char i=69;i>=65;i--)
+  {
+  for(char j=65;j<=i;j++)
+  {
+  System.out.print(j+" ");
+  }
+  System.out.println("");
+  }
+  }
+  }
