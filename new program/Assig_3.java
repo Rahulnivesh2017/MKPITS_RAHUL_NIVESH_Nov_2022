d2f5c1ff02ff27231c1e1c42d100d5939799e552
rahul nivesh <rahulnivesh2017@gmail.com>
2023-03-08 23:41:02 +0530

    assig_4

---
+++ new program/Assig_3.java
@@ -0,0 +1,41 @@
+// Write a C program to to test if a given non-negative number is a multiple of 13 or it is one more than a multiple of 13.
+
+import java.util.*;
+
+class Assig_3{
+
+
+public static void main(String [] argu){
+
+Scanner sc  = new Scanner(System.in);
+
+System.out.println("eneter the num");
+int a = sc.nextInt();
+
+if(a>0)
+{
+
+if(a%13==0)
+{
+
+	System.out.println("It is a multiple of 13");
+
+	}
+else
+{
+
+		System.out.println("Not a multiple of 13");
+
+	}
+
+}
+else {
+
+	System.out.println("NUm should be greater that zero");
+
+	}
+
+
+}
+
+}
