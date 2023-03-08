8832730079d1e2a573f9206151be95d677c54cff
rahul nivesh <rahulnivesh2017@gmail.com>
2023-03-08 23:08:04 +0530

    pattern_char1

---
+++ new program/pattern_char1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class pattern_char1
+{
+public static void main(String []args)
+{
+
+ System.out.println("enter value of 5");
+
+
+  for(char i='A';i<='E';i++)
+  {
+  for(char j='A';j<=i;j++){
+
+  System.out.print(j+" ");
+  }
+  System.out.println(" ");
+  }
+  }
+  }
