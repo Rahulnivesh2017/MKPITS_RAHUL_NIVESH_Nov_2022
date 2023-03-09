af3bd039e119bb5b65cd57477d72156a46872ce7
rahul nivesh <rahulnivesh2017@gmail.com>
2023-03-10 00:05:41 +0530

    Stack_13

---
+++ new program/Stack_13.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+class Stack_13{
+
+public static void main(String []args){
+
+Stack s1=new Stack();
+
+System.out.println("size"+s1.size());
+
+s1.push(11);
+s1.push(26);
+s1.push(40);
+s1.push(20);
+s1.push(11);
+
+System.out.println("size"+s1.size());
+
+System.out.println(s1);
+
+}
+}
