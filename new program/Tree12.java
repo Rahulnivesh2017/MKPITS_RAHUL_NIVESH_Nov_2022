cca9f91db962833af182fa1c96dd2b4888a77684
rahul nivesh <rahulnivesh2017@gmail.com>
2023-03-09 23:57:17 +0530

    TreeMap

---
+++ new program/Tree12.java
@@ -0,0 +1,21 @@
+import java.util.TreeMap;
+
+class Tree12{
+
+public static void main(String []args){
+
+TreeMap tm=new TreeMap();
+
+System.out.println("size"+tm.size());
+
+tm.put(1,"rahul");
+tm.put(2,"akash");
+tm.put(3,"zaffar");
+tm.put(4,"roman");
+
+System.out.println("size"+tm.size());
+
+System.out.println(tm);
+
+}
+}
