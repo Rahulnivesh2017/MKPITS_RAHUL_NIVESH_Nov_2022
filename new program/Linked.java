de2e94519a729fb8cff5521ea7034ca5d99a3766
rahul nivesh <rahulnivesh2017@gmail.com>
2023-03-09 23:34:00 +0530

    Linked

---
+++ new program/Linked.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+
+class Linked{
+public static void main(String []args){
+LinkedList li=new LinkedList ();
+System.out.println("list"+li.size());
+li.add(20);
+li.add(5);
+li.add(6);
+li.add(1);
+li.add(3);
+li.add(12);
+li.add(32);
+System.out.println("list"+li.size());
+System.out.println(li);
+}
+}
