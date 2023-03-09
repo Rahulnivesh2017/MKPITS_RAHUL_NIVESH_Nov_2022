199c5f112269be6c2f87f5483b0ff70e226f2ea2
rahul nivesh <rahulnivesh2017@gmail.com>
2023-03-10 00:35:21 +0530

    linked

---
+++ new program/Linked_char.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+
+class Linked_char{
+	
+public static void main(String []args){
+	
+LinkedList li=new LinkedList ();
+
+System.out.println("list"+li.size());
+
+li.add("rahul");
+li.add("Akasha");
+li.add("Sachin");
+li.add("Anmol");
+li.add("ram");
+li.add("Amit");
+li.add("karuna");
+
+System.out.println("list"+li.size());
+
+System.out.println(li);
+
+}
+}
