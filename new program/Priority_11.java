4b3c2a9ccb689c143bc67ace7b0f64a30b812e00
rahul nivesh <rahulnivesh2017@gmail.com>
2023-03-09 23:52:49 +0530

    priorityQueue

---
+++ new program/Priority_11.java
@@ -0,0 +1,24 @@
+import java.util.PriorityQueue;
+
+class Priority_11{
+
+public static void main(String []args){
+
+PriorityQueue pq=new PriorityQueue();
+
+System.out.println("size" +pq.size());
+
+pq.add(35);
+pq.add(5);
+pq.add(9);
+pq.add(13);
+pq.add(50);
+pq.add(90);
+
+
+System.out.println("size" +pq.size());
+
+System.out.println(pq);
+
+}
+}
