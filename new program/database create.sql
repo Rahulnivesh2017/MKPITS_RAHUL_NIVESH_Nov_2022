5d57b22537b635ba275da764a4bf43413bdbf647
rahul nivesh <rahulnivesh2017@gmail.com>
2023-03-19 22:59:33 +0530

    database

---
+++ new program/database create.sql
@@ -0,0 +1,49 @@
+// create database 
+
+
+mysql> create database csegyan;
+Query OK, 1 row affected (0.01 sec)
+
+mysql> show databases;
++----------------------+
+| Database             |
++----------------------+
+| information_schema   |
+| b1                   |
+| bharat               |
+| csegyan              |
+| empdb                |
+| employee             |
+| emprr                |
+| familyaaa            |
+| familydb             |
+| familyrn             |
+| login1               |
+| login2               |
+| login3               |
+| login5               |
+| logindemo            |
+| memoryab             |
+| myfamily             |
+| myfamilyan           |
+| myfamilydb           |
+| myfriends            |
+| myrahul              |
+| mysql                |
+| newfamily            |
+| niveshab             |
+| pawan                |
+| performance_schema   |
+| practice_database    |
+| rahul22              |
+| rahulss              |
+| somaliadb            |
+| somalidb2            |
+| structure_inventory1 |
+| structure_inventry   |
+| structure_moviesdb   |
+| student11            |
+| test                 |
+| vishal               |
++----------------------+
+37 rows in set (0.00 sec)
