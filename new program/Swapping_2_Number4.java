0ed11b1b17319c2109dc615a550f855f0e62f3d0
rahul nivesh <rahulnivesh2017@gmail.com>
2023-04-03 23:48:08 +0530

    Swapping

---
+++ new program/Swapping_2_Number4.java
@@ -0,0 +1,11 @@
+class Swapping_2_Number4{
+public static void main(String []args)
+{
+  int a=10,b=20;
+  System.out.println("Befor swapping values are"+a+ " "+b);
+  //logic-5-single statement
+  // a=10 b=20
+  b=a+b-(a=b);
+   System.out.println("After swapping values are"+a+ " "+b);
+   }
+   }
