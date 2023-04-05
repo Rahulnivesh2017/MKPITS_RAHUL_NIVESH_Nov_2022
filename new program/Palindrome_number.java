e411c0674f6f39cfec4f5565ab3d9bb80167e9db
rahul nivesh <rahulnivesh2017@gmail.com>
2023-04-05 23:31:23 +0530

    palindrom

--- new program/Palindrome_number.java
+++ new program/Palindrome_number.java
@@ -1,42 +1,28 @@
-import java.util.Scanner;
+// Palindrome Number
 
+import java.util.*;
 
-class Palindrome_number
+class Palindrome_Number{
+public static void main(String []args)
 {
- public static void main(String []args)
- {
- int num,n1,n2,n3,n4,n5,ori,rev;
- System.out.println("Enter 5 digit number");
- Scanner sc=new Scanner(System.in);
- num=sc.nextInt();
+  Scanner sc=new Scanner(System.in);
+  System.out.println("Enter a number");
+  int num=sc.nextInt();
 
- //seperate logic
- ori=num;
- n1=num%10;
- num=num/10;
- n2=num%10;
- num=num/10;
- n3=num%10;
- num=num/10;
- n4=num%10;
- num=num/10;
- n5=num%10;
- num=num/10;
-
- //reverse logic
- rev=n1*10000+n2*1000+n3*100+n4*10+n5*1;
-
- //loop
- if(rev == ori)
- {
- System.out.println("palindrome");
- }
- else
- {
- System.out.println("not palindrome");
- }
- }
- }
-
-
-
+  int org_num=num;
+  int rev=0;
+  while(num!=0)
+  {
+    rev=rev*10+num%10;
+    num=num/10;
+    }
+    if(org_num==rev)
+    {
+      System.out.println(org_num+" Palindrome Number");
+      }
+      else
+      {
+       System.out.println(org_num+"  not Palindrome Number");
+       }
+       }
+       }
