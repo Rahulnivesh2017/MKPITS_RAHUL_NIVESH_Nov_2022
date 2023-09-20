1bd11f088d6c56988958ee3b313406a011447c22
rahul nivesh <rahulnivesh2017@gmail.com>
2023-09-20 09:37:22 +0530

    Loop Program

---
+++ While1.java
@@ -0,0 +1,26 @@
+
+
+  // While Loop
+
+      import java.util.Scanner;
+
+     class While1{
+
+        public static void main(String[] args){
+
+            int n=1;
+
+              System.out.println("Enter value for condition \n ");
+
+                 Scanner sc=new Scanner(System.in);
+                   n=sc.nextInt();
+
+                      while(n<=10)
+                      {
+                         System.out.println("Rahul with Samiksha");
+                         ++n;
+                       }
+
+                     }
+
+                 }
